f4d32b1864730109f818bdac4c8ba509cbc732fc
khauri <kmcclain@email.wm.edu>
2021-06-08 11:35:48 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import indexPage from "./pages/index";
-import usersService from "./services/users";
 
 export default Router()
-  .get("/", indexPage)
-  .get("/services/users", usersService);
+  .get("/", indexPage);
